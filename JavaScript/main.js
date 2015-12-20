a1301667eef92aa87b0e4741fc30da571079006b
mamian <mamian521@gmail.com>
2015-12-20 14:03:17 +0800

    增加所有

---
+++ JavaScript/main.js
@@ -0,0 +1,32 @@
+/**
+ * Created by MAMIAN on 15/11/4.
+ */
+
+$(document).ready(function(){
+
+    dropdownOpen();//调用
+
+});
+
+/**
+ 05
+ * 鼠标划过就展开子菜单，免得需要点击才能展开
+ 06
+ */
+
+function dropdownOpen() {
+
+
+
+    var $dropdownLi = $('li.dropdown');
+    $dropdownLi.mouseover(function() {
+
+        $(this).addClass('open');
+
+    }).mouseout(function() {
+
+        $(this).removeClass('open');
+
+    });
+
+}
